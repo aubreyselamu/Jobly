18828c4044c9f59093d76a91a636e4dc9a1b6208
Aubrey Selamu-Bell <aubrey.selamu@Aubreys-MBP.localdomain>
2022-06-15 14:29:24 -0400

    update /companies route to allow for optional filtering through query string

--- routes/companies.js
+++ routes/companies.js
@@ -11,6 +11,7 @@ const Company = require("../models/company");
 
 const companyNewSchema = require("../schemas/companyNew.json");
 const companyUpdateSchema = require("../schemas/companyUpdate.json");
+const companySearchSchema = require("../schemas/companySearchSchema.json");
 
 const router = new express.Router();
 
@@ -51,9 +52,22 @@ router.post("/", ensureLoggedIn, async function (req, res, next) {
  */
 
 router.get("/", async function (req, res, next) {
-  try {
-    const companies = await Company.findAll();
-    return res.json({ companies });
+    let q = req.query;
+
+    //Arrives as string from query, but want as integer
+    if(q.minEmployees !== undefined) q.minEmployees = +minEmployees;
+    if(q.maxEmployees !== undefined) q.maxEmployees = +maxEmployees;
+
+    try {
+      const validator = jsonschema.validate(q, companySearchSchema)
+      if(!validator.valid){
+        const errs = validator.errors.map(e => e.stack);
+        throw new BadRequestError(errs)
+      }
+
+      const companies = await Company.findAll(q);
+      return res.json({ companies });
+
   } catch (err) {
     return next(err);
   }
